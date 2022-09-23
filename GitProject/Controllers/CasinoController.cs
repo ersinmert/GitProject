eaa7867c29e7bbe45bd8280a66cbd107b9160621
Ersin Mert Demircan <e.mertdemircan@gmail.com>
2022-09-23 16:42:10 +0300

    Casino controller added!

---
+++ GitProject/Controllers/CasinoController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GitProject.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CasinoController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok("casino ok!");
+        }
+    }
+}
